9015af91b079c68d0bb1b6eae9d7198259aa5ffc
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-13 14:33:59 -0600

    Created assignment 2 and made Node class

---
+++ Morgan_Assignment02/src/linkedlists/Node.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package linkedlists;
+
+/**
+ *
+ * @author Wyatt Morgan
+ */
+public class Node {
+    int std_ID;
+    String firstName;
+    String lastName;
+    boolean isGradOrNot;
+    int age;
+    Node next;
+    
+    public Node(int std_ID, String firstName, String lastName, boolean isGradOrNot, int age){
+        this.std_ID = std_ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isGradOrNot = isGradOrNot;
+        this.age = age;
+    }
+    
+    
+}
