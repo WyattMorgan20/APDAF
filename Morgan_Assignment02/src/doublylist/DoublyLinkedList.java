7c3c53b3839e2d8f8d4c3d6ce22d13fc0446470c
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-16 16:34:48 -0600

    finished part 2

--- Morgan_Assignment02/src/doublylist/DoublyLinkedList.java
+++ Morgan_Assignment02/src/doublylist/DoublyLinkedList.java
@@ -10,6 +10,62 @@ package doublylist;
  */
 public class DoublyLinkedList {
     
+    public Node head;
     
+    public Node insertNode(Node head, Node newNode){
+        if(head == null){
+            this.head = newNode;
+            return this.head;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        newNode.previous = temp;
+        return this.head;
+        
+    }
     
+    public void print(Node head){
+        Node temp = head;
+        System.out.println("******************************************");
+        System.out.println("Doubly Linked List:");
+        while (temp != null) {
+            System.out.println("The student ID is: " + temp.std_ID);
+            System.out.println("First Name: " + temp.firstName);
+            System.out.println("Last Name: " + temp.lastName);
+            System.out.println("isGraduated: " + temp.isGradOrNot);
+            System.out.println("Age: " + temp.age);
+            System.out.println("******");
+            temp = temp.next;
+        }
+       
+    }
+    
+    public void reverseList(Node head){
+        System.out.println("***********************************");
+        System.out.println("The linked list printed in reverse order is");
+        Node temp = head;
+        
+        if(temp == null){
+            return;
+        }
+        
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        
+        while(temp != null){
+            System.out.println("The student ID is: " + temp.std_ID);
+            System.out.println("First Name: " + temp.firstName);
+            System.out.println("Last Name: " + temp.lastName);
+            System.out.println("isGraduated: " + temp.isGradOrNot);
+            System.out.println("Age: " + temp.age);
+            System.out.println("******");
+            
+            temp = temp.previous;
+        }
+        
+    }
 }
--- Morgan_Assignment02/src/doublylist/DriverClass.java
+++ Morgan_Assignment02/src/doublylist/DriverClass.java
@@ -4,6 +4,8 @@
  */
 package doublylist;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Wyatt Morgan
@@ -11,7 +13,37 @@ package doublylist;
 public class DriverClass {
     
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        DoublyLinkedList doublylinkedlist = new DoublyLinkedList();
         
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter student ID: ");
+            int sID = input.nextInt();
+            input.nextLine();
+            
+            System.out.print("Enter first name: ");
+            String firstName = input.nextLine();
+            
+            System.out.print("Enter last name: ");
+            String lastName = input.nextLine();
+            
+            System.out.print("Enter student is graduated?: ");
+            boolean grad = input.nextBoolean();
+            
+            System.out.print("Enter student age: ");
+            int age = input.nextInt();
+            input.nextLine();
+            
+            Node newNode = new Node(sID, firstName, lastName, grad, age);
+            doublylinkedlist.head = doublylinkedlist.insertNode(doublylinkedlist.head, newNode);
+            
+            System.out.println("");
+            System.out.println("");
+        }
+        
+        doublylinkedlist.print(doublylinkedlist.head);
+        doublylinkedlist.reverseList(doublylinkedlist.head);
+        input.close();
     }
     
 }
