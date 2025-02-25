b8ea6b00746d63a3c061958e267ed393c84bf099
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-25 13:26:09 -0600

    Started assignment3

---
+++ Morgan_Assignment03/src/stack/ValidParenthesesClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stack;
+import java.util.Scanner;
+/**
+ *
+ * @author Wyatt Morgan
+ */
+public class ValidParenthesesClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter String: ");
+        String userInput = input.nextLine();
+    }
+    
+}
