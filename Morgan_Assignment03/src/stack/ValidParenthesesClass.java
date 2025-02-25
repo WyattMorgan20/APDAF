1797e6da3dc7a303b20c6cc3bfb707d90ef0f518
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-25 13:50:44 -0600

    made beginning implementation of assignment 3

--- Morgan_Assignment03/src/stack/ValidParenthesesClass.java
+++ Morgan_Assignment03/src/stack/ValidParenthesesClass.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package stack;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 /**
  *
  * @author Wyatt Morgan
@@ -16,8 +18,32 @@ public class ValidParenthesesClass {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Enter String: ");
+        System.out.print("Enter any string: ");
         String userInput = input.nextLine();
+        
+        Stack<String> operands = new Stack<>();
+        Stack<String> operators = new Stack<>();
+        Stack<String> braces = new Stack<>();
+        
+        for(int i = 0; i < userInput.length(); i++){
+            char ch = userInput.charAt(i);
+            String userInputStr = String.valueOf(ch);
+            if(Character.isLetterOrDigit(ch)){
+                operands.push(userInputStr);
+            }
+            else if("+-*/%".contains(userInputStr)){
+                operators.push(userInputStr);
+            }
+            else if("()[]{}".contains(userInputStr)){
+                braces.push(userInputStr);
+            }
+        }
+        
+        System.out.println("\nOperands stack: " + operands);
+        System.out.println("Operators stack: " + operators);
+        System.out.println("Braces stack: " + braces);
+        
+        
     }
     
 }
