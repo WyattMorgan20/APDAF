bcbf44e2c06deb8616691b5894a63e3700391cfd
Wyatt Morgan <iamtheyy@gmail.com>
2025-03-26 16:30:11 -0500

    created and started assignment 4

---
+++ Morgan_Assignment04/src/Set/SetPerformanceComp.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Wyatt Morgan
+ */
+public class SetPerformanceComp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void testWithSize(int size){
+        Set<Integer> hashSet = new HashSet<>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        Set<Integer> treeSet = new TreeSet<>();
+        
+        
+    }
+    
+}
